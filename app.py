da89a10c7cf5d32158ead97443083f5a20416bc8
HendrikPeru <90179624+HendrikPeru@users.noreply.github.com>
2023-12-19 21:53:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,46 @@
 from flask import Flask, render_template, request
+from geopy.geocoders import Nominatim
+from geopy.distance import great_circle
 
 app = Flask(__name__)
+
+def saa_koordinaadid(koht):
+    geokooder = Nominatim(user_agent="minu_rakendus")
+    asukoht = geokooder.geocode(koht)
+
+    if asukoht:
+        laiuskraad = asukoht.latitude
+        pikkuskraad = asukoht.longitude
+        return laiuskraad, pikkuskraad
+    else:
+        return None
+
+def vahemaa(asukoht1, asukoht2):
+    kaugus = great_circle(asukoht1, asukoht2).kilometers
+    return kaugus
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/submit', methods=['POST'])
-def submit():
+@app.route('/calculate', methods=['POST'])
+def calculate():
     if request.method == 'POST':
-        location = request.form['location']
-        fuel_type = request.form['fuel_type']
-        return f"Valitud asukoht: {location}, Valitud kütus: {fuel_type}"
-    return "Midagi läks valesti!"
+        koht1 = request.form['koht1']
+        koht2 = request.form['koht2']
+        
+        asukoht1 = saa_koordinaadid(koht1)
+        asukoht2 = saa_koordinaadid(koht2)
+        
+        if asukoht1 and asukoht2:
+            kaugus = vahemaa(asukoht1, asukoht2)
+            if kaugus < 1:
+                tulemus = f"Kahe asukoha vaheline kaugus: {round(kaugus*1000)} meetrit"
+            else:
+                tulemus = f"Kahe asukoha vaheline kaugus: {round(kaugus, 2)} kilomeetrit"
+            return render_template('result.html', tulemus=tulemus)
+        else:
+            return "Üks või mõlemad asukohad ei leitud."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True
